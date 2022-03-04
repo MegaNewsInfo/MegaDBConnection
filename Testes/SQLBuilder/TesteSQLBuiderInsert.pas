a234d81f4ac275bc3c1c772060cfa0adc545bb8c
Mauricio <mauricio.barreto@meganews.com.br>
2022-03-04 15:36:56 -0300

    Conclusão dos testes da classe SQLBuilderInsert

--- Testes/SQLBuilder/TesteSQLBuiderInsert.pas
+++ Testes/SQLBuilder/TesteSQLBuiderInsert.pas
@@ -14,6 +14,26 @@ type
   procedure TesteAdicionaSemQuoted;
   [Test]
   procedure TesteAdicionaComQuoted;
+  [Test]
+  procedure TesteAddStringComTamanho;
+  [Test]
+  procedure TesteAddStringSemTamanho;
+  [Test]
+  procedure TesteAddComAdicionarParametro;
+  [Test]
+  procedure TesteAddSemAdicionarParametro;
+  [Test]
+  procedure TesteAddBooleanTrue;
+  [Test]
+  procedure TesteAddBooleanFalse;
+  [Test]
+  procedure TesteAddNull;
+
+  [Test]
+  procedure TesteCriterio;
+  [Test]
+  procedure TesteTabela;
+
 
 
   end;
@@ -22,6 +42,100 @@ implementation
 
 
 { TesteSQLBuiderInsert }
+
+procedure TTesteSQLBuiderInsert.TesteAddBooleanFalse;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''N'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste',False).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddBooleanTrue;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''S'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste',True).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddComAdicionarParametro;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''MEGA'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste','MEGA',0,True).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddNull;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+ lNul :Variant;
+begin
+
+  lsql := 'insert into  (teste) values (NULL) ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste',lNul).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddSemAdicionarParametro;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''MEGA'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste','MEGA',0,True).
+           Add('teste2','COISA',0,False).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddStringComTamanho;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''MEG'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste','MEGA',3).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteAddStringSemTamanho;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+
+  lsql := 'insert into  (teste) values (''MEGA'') ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste','MEGA').ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
 
 procedure TTesteSQLBuiderInsert.TesteAdicionaComQuoted;
 var
@@ -45,6 +159,30 @@ begin
   lSqlBuilder := TSqlBuilderInsert.Create;
   lsql2 := StringReplace(lSqlBuilder.Add('teste',1).ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
   Assert.AreEqual(lsql.Trim,Trim(lsql2));
+end;
+
+procedure TTesteSQLBuiderInsert.TesteCriterio;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+  lsql := 'insert into  (teste) values (1) ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste',1).Criterio('where').ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
+end;
+
+procedure TTesteSQLBuiderInsert.TesteTabela;
+var
+ lSqlBuilder : iSqlBuilder;
+ lsql,lsql2 : string;
+begin
+  lsql := 'insert into FERA (teste) values (1) ;';
+  lSqlBuilder := TSqlBuilderInsert.Create;
+  lsql2 := StringReplace(lSqlBuilder.Add('teste',1).Tabela('FERA').ToString.Trim,sLineBreak,'',[rfReplaceAll,rfIgnoreCase]);
+  Assert.AreEqual(lsql.Trim,Trim(lsql2));
+
 end;
 
 initialization
